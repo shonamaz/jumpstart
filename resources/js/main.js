9a9316a3e40f174efeddbd3aabac5f37448bb881
shonamaz <samasarin@gmail.com>
2018-06-18 12:13:40 -0400

    Add navbar animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+// Jumbotron Image Carousel
   $('.carousel').slick({
     autoplay: true,
     arrows: false,
@@ -8,4 +10,28 @@ $(document).ready(function(){
     pauseOnFocus: false,
 
   });
+
+//Scroll Function
+
+  var scrollTopV = 0
+
+$(window).scroll (function(){
+  var currentScrollPosition = $(this).scrollTop();
+  if (currentScrollPosition - scrollTopV > 50) {
+  var navBarHeight = $('.navbar').css('height');
+  $( ".navbar" ).animate({top: '-' + navBarHeight}, 150);
+  scrollTopV = currentScrollPosition;
+} else if (scrollTopV - currentScrollPosition > 50) {
+  $(".navbar").animate({top: '0px'}, 150);
+  scrollTopV = currentScrollPosition;
+}
+  });
+
+
+
+
+
+
+
+
 });
