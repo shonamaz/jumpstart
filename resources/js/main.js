8173b8151d1cd2301438f64a5cdcba022b3a3fb2
shonamaz <samasarin@gmail.com>
2018-06-18 10:22:13 -0400

    Add imag carousel

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    arrows: false,
+    draggable: false,
+    speed: 2000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+
+  });
+});
